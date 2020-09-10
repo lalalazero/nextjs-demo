9ea2836705c0fe70ebc6b52df40b1200df99339d
lalalazero <zzzero520@hotmail.com>
2020-09-10 09:57:27 +0800

    dynamic routes 动态路由

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -14,16 +14,18 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hi there.</p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}> 
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
